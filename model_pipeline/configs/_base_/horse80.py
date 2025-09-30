d334ba9257110d08385faee4b615cab614a78e9f
Amanda Mårtensson <amanda.martensson@outlook.com>
2025-09-30 11:02:28 +0200

    Updated config names and files for 80 bones intead of 68

---
+++ model_pipeline/configs/_base_/horse80.py
@@ -0,0 +1,183 @@
+# AUTO-GENERATED FILE: horse80.py
+# Correct metainfo for horse80 (80 keypoints, incl. mane joints)
+
+dataset_info = dict(
+    dataset_name='horse80',
+    paper_info=dict(
+        author='KinEquiMation',
+        title='Horse Pose Estimation (80 keypoints)',
+        year=2025,
+        homepage='https://github.com/xSomelime/KinEquiMation',
+    ),
+
+    # ---- Keypoints ----
+    keypoint_info={
+        0: dict(name='DEF-breast.L', id=0, color=[0,255,0], type='upper', swap='DEF-breast.R'),
+        1: dict(name='DEF-breast.R', id=1, color=[0,255,0], type='upper', swap='DEF-breast.L'),
+        2: dict(name='DEF-chest', id=2, color=[0,255,0], type='upper', swap=''),
+        3: dict(name='DEF-ear.L', id=3, color=[0,255,0], type='upper', swap='DEF-ear.R'),
+        4: dict(name='DEF-ear.L.001', id=4, color=[0,255,0], type='upper', swap='DEF-ear.R.001'),
+        5: dict(name='DEF-ear.R', id=5, color=[0,255,0], type='upper', swap='DEF-ear.L'),
+        6: dict(name='DEF-ear.R.001', id=6, color=[0,255,0], type='upper', swap='DEF-ear.L.001'),
+        7: dict(name='DEF-eye.L', id=7, color=[0,255,0], type='upper', swap='DEF-eye.R'),
+        8: dict(name='DEF-eye.R', id=8, color=[0,255,0], type='upper', swap='DEF-eye.L'),
+        9: dict(name='DEF-f_hoof.L', id=9, color=[0,255,0], type='lower', swap='DEF-f_hoof.R'),
+        10: dict(name='DEF-f_hoof.R', id=10, color=[0,255,0], type='lower', swap='DEF-f_hoof.L'),
+        11: dict(name='DEF-f_toe.L', id=11, color=[0,255,0], type='lower', swap='DEF-f_toe.R'),
+        12: dict(name='DEF-f_toe.R', id=12, color=[0,255,0], type='lower', swap='DEF-f_toe.L'),
+        13: dict(name='DEF-forearm.L', id=13, color=[0,255,0], type='upper', swap='DEF-forearm.R'),
+        14: dict(name='DEF-forearm.L.001', id=14, color=[0,255,0], type='upper', swap='DEF-forearm.R.001'),
+        15: dict(name='DEF-forearm.R', id=15, color=[0,255,0], type='upper', swap='DEF-forearm.L'),
+        16: dict(name='DEF-forearm.R.001', id=16, color=[0,255,0], type='upper', swap='DEF-forearm.L.001'),
+        17: dict(name='DEF-forefoot.L', id=17, color=[0,255,0], type='upper', swap='DEF-forefoot.R'),
+        18: dict(name='DEF-forefoot.L.001', id=18, color=[0,255,0], type='upper', swap='DEF-forefoot.R.001'),
+        19: dict(name='DEF-forefoot.R', id=19, color=[0,255,0], type='upper', swap='DEF-forefoot.L'),
+        20: dict(name='DEF-forefoot.R.001', id=20, color=[0,255,0], type='upper', swap='DEF-forefoot.L.001'),
+        21: dict(name='DEF-head', id=21, color=[0,255,0], type='upper', swap=''),
+        22: dict(name='DEF-hind_foot.L', id=22, color=[0,255,0], type='upper', swap='DEF-hind_foot.R'),
+        23: dict(name='DEF-hind_foot.L.001', id=23, color=[0,255,0], type='upper', swap='DEF-hind_foot.R.001'),
+        24: dict(name='DEF-hind_foot.R', id=24, color=[0,255,0], type='upper', swap='DEF-hind_foot.L'),
+        25: dict(name='DEF-hind_foot.R.001', id=25, color=[0,255,0], type='upper', swap='DEF-hind_foot.L.001'),
+        26: dict(name='DEF-jaw', id=26, color=[0,255,0], type='upper', swap=''),
+        27: dict(name='DEF-jaw.001', id=27, color=[0,255,0], type='upper', swap=''),
+        28: dict(name='DEF-lower_leg.L', id=28, color=[0,255,0], type='lower', swap='DEF-lower_leg.R'),
+        29: dict(name='DEF-lower_leg.L.001', id=29, color=[0,255,0], type='lower', swap='DEF-lower_leg.R.001'),
+        30: dict(name='DEF-lower_leg.R', id=30, color=[0,255,0], type='lower', swap='DEF-lower_leg.L'),
+        31: dict(name='DEF-lower_leg.R.001', id=31, color=[0,255,0], type='lower', swap='DEF-lower_leg.L.001'),
+        32: dict(name='DEF-mane_base.01', id=32, color=[0,255,0], type='upper', swap=''),
+        33: dict(name='DEF-mane_base.02', id=33, color=[0,255,0], type='upper', swap=''),
+        34: dict(name='DEF-mane_base.03', id=34, color=[0,255,0], type='upper', swap=''),
+        35: dict(name='DEF-mane_base.04', id=35, color=[0,255,0], type='upper', swap=''),
+        36: dict(name='DEF-mane_base.05', id=36, color=[0,255,0], type='upper', swap=''),
+        37: dict(name='DEF-mane_base.06', id=37, color=[0,255,0], type='upper', swap=''),
+        38: dict(name='DEF-mane_top.01', id=38, color=[0,255,0], type='upper', swap=''),
+        39: dict(name='DEF-mane_top.02', id=39, color=[0,255,0], type='upper', swap=''),
+        40: dict(name='DEF-mane_top.03', id=40, color=[0,255,0], type='upper', swap=''),
+        41: dict(name='DEF-mane_top.04', id=41, color=[0,255,0], type='upper', swap=''),
+        42: dict(name='DEF-mane_top.05', id=42, color=[0,255,0], type='upper', swap=''),
+        43: dict(name='DEF-mane_top.06', id=43, color=[0,255,0], type='upper', swap=''),
+        44: dict(name='DEF-neck.001', id=44, color=[0,255,0], type='upper', swap=''),
+        45: dict(name='DEF-neck.002', id=45, color=[0,255,0], type='upper', swap=''),
+        46: dict(name='DEF-neck.003', id=46, color=[0,255,0], type='upper', swap=''),
+        47: dict(name='DEF-neck.004', id=47, color=[0,255,0], type='upper', swap=''),
+        48: dict(name='DEF-nose.L', id=48, color=[0,255,0], type='upper', swap='DEF-nose.R'),
+        49: dict(name='DEF-nose.R', id=49, color=[0,255,0], type='upper', swap='DEF-nose.L'),
+        50: dict(name='DEF-pelvis.L', id=50, color=[0,255,0], type='lower', swap='DEF-pelvis.R'),
+        51: dict(name='DEF-pelvis.R', id=51, color=[0,255,0], type='lower', swap='DEF-pelvis.L'),
+        52: dict(name='DEF-r_hoof.L', id=52, color=[0,255,0], type='lower', swap='DEF-r_hoof.R'),
+        53: dict(name='DEF-r_hoof.R', id=53, color=[0,255,0], type='lower', swap='DEF-r_hoof.L'),
+        54: dict(name='DEF-r_toe.L', id=54, color=[0,255,0], type='lower', swap='DEF-r_toe.R'),
+        55: dict(name='DEF-r_toe.R', id=55, color=[0,255,0], type='lower', swap='DEF-r_toe.L'),
+        56: dict(name='DEF-shoulder.L', id=56, color=[0,255,0], type='upper', swap='DEF-shoulder.R'),
+        57: dict(name='DEF-shoulder.R', id=57, color=[0,255,0], type='upper', swap='DEF-shoulder.L'),
+        58: dict(name='DEF-skull', id=58, color=[0,255,0], type='upper', swap=''),
+        59: dict(name='DEF-skull.L', id=59, color=[0,255,0], type='upper', swap='DEF-skull.R'),
+        60: dict(name='DEF-skull.R', id=60, color=[0,255,0], type='upper', swap='DEF-skull.L'),
+        61: dict(name='DEF-spine.001', id=61, color=[0,255,0], type='upper', swap=''),
+        62: dict(name='DEF-spine.002', id=62, color=[0,255,0], type='upper', swap=''),
+        63: dict(name='DEF-spine.003', id=63, color=[0,255,0], type='upper', swap=''),
+        64: dict(name='DEF-spine.004', id=64, color=[0,255,0], type='upper', swap=''),
+        65: dict(name='DEF-spine.005', id=65, color=[0,255,0], type='upper', swap=''),
+        66: dict(name='DEF-spine.006', id=66, color=[0,255,0], type='upper', swap=''),
+        67: dict(name='DEF-tail.001', id=67, color=[0,255,0], type='upper', swap=''),
+        68: dict(name='DEF-tail.002', id=68, color=[0,255,0], type='upper', swap=''),
+        69: dict(name='DEF-tail.003', id=69, color=[0,255,0], type='upper', swap=''),
+        70: dict(name='DEF-tail.004', id=70, color=[0,255,0], type='upper', swap=''),
+        71: dict(name='DEF-tail.005', id=71, color=[0,255,0], type='upper', swap=''),
+        72: dict(name='DEF-thigh.L', id=72, color=[0,255,0], type='upper', swap='DEF-thigh.R'),
+        73: dict(name='DEF-thigh.L.001', id=73, color=[0,255,0], type='upper', swap='DEF-thigh.R.001'),
+        74: dict(name='DEF-thigh.R', id=74, color=[0,255,0], type='upper', swap='DEF-thigh.L'),
+        75: dict(name='DEF-thigh.R.001', id=75, color=[0,255,0], type='upper', swap='DEF-thigh.L.001'),
+        76: dict(name='DEF-upper_arm.L', id=76, color=[0,255,0], type='upper', swap='DEF-upper_arm.R'),
+        77: dict(name='DEF-upper_arm.L.001', id=77, color=[0,255,0], type='upper', swap='DEF-upper_arm.R.001'),
+        78: dict(name='DEF-upper_arm.R', id=78, color=[0,255,0], type='upper', swap='DEF-upper_arm.L'),
+        79: dict(name='DEF-upper_arm.R.001', id=79, color=[0,255,0], type='upper', swap='DEF-upper_arm.L.001'),
+    },
+
+    # ---- Skeleton edges ----
+    skeleton_info={
+        0: dict(link=('DEF-spine.001','DEF-spine.002'), color=[255,0,0]),
+        1: dict(link=('DEF-spine.002','DEF-spine.003'), color=[255,0,0]),
+        2: dict(link=('DEF-spine.003','DEF-spine.004'), color=[255,0,0]),
+        3: dict(link=('DEF-spine.004','DEF-spine.005'), color=[255,0,0]),
+        4: dict(link=('DEF-spine.005','DEF-spine.006'), color=[255,0,0]),
+        5: dict(link=('DEF-spine.006','DEF-neck.001'), color=[255,0,0]),
+        6: dict(link=('DEF-neck.001','DEF-neck.002'), color=[255,0,0]),
+        7: dict(link=('DEF-neck.002','DEF-neck.003'), color=[255,0,0]),
+        8: dict(link=('DEF-neck.003','DEF-neck.004'), color=[255,0,0]),
+        9: dict(link=('DEF-neck.004','DEF-head'), color=[255,0,0]),
+        10: dict(link=('DEF-tail.001','DEF-tail.002'), color=[255,0,0]),
+        11: dict(link=('DEF-tail.002','DEF-tail.003'), color=[255,0,0]),
+        12: dict(link=('DEF-tail.003','DEF-tail.004'), color=[255,0,0]),
+        13: dict(link=('DEF-tail.004','DEF-tail.005'), color=[255,0,0]),
+        14: dict(link=('DEF-thigh.L','DEF-thigh.L.001'), color=[255,0,0]),
+        15: dict(link=('DEF-thigh.L.001','DEF-lower_leg.L'), color=[255,0,0]),
+        16: dict(link=('DEF-lower_leg.L','DEF-lower_leg.L.001'), color=[255,0,0]),
+        17: dict(link=('DEF-lower_leg.L.001','DEF-hind_foot.L'), color=[255,0,0]),
+        18: dict(link=('DEF-hind_foot.L','DEF-hind_foot.L.001'), color=[255,0,0]),
+        19: dict(link=('DEF-hind_foot.L.001','DEF-r_toe.L'), color=[255,0,0]),
+        20: dict(link=('DEF-r_toe.L','DEF-r_hoof.L'), color=[255,0,0]),
+        21: dict(link=('DEF-thigh.R','DEF-thigh.R.001'), color=[255,0,0]),
+        22: dict(link=('DEF-thigh.R.001','DEF-lower_leg.R'), color=[255,0,0]),
+        23: dict(link=('DEF-lower_leg.R','DEF-lower_leg.R.001'), color=[255,0,0]),
+        24: dict(link=('DEF-lower_leg.R.001','DEF-hind_foot.R'), color=[255,0,0]),
+        25: dict(link=('DEF-hind_foot.R','DEF-hind_foot.R.001'), color=[255,0,0]),
+        26: dict(link=('DEF-hind_foot.R.001','DEF-r_toe.R'), color=[255,0,0]),
+        27: dict(link=('DEF-r_toe.R','DEF-r_hoof.R'), color=[255,0,0]),
+        28: dict(link=('DEF-upper_arm.L','DEF-upper_arm.L.001'), color=[255,0,0]),
+        29: dict(link=('DEF-upper_arm.L.001','DEF-forearm.L'), color=[255,0,0]),
+        30: dict(link=('DEF-forearm.L','DEF-forearm.L.001'), color=[255,0,0]),
+        31: dict(link=('DEF-forearm.L.001','DEF-forefoot.L'), color=[255,0,0]),
+        32: dict(link=('DEF-forefoot.L','DEF-forefoot.L.001'), color=[255,0,0]),
+        33: dict(link=('DEF-forefoot.L.001','DEF-f_toe.L'), color=[255,0,0]),
+        34: dict(link=('DEF-f_toe.L','DEF-f_hoof.L'), color=[255,0,0]),
+        35: dict(link=('DEF-upper_arm.R','DEF-upper_arm.R.001'), color=[255,0,0]),
+        36: dict(link=('DEF-upper_arm.R.001','DEF-forearm.R'), color=[255,0,0]),
+        37: dict(link=('DEF-forearm.R','DEF-forearm.R.001'), color=[255,0,0]),
+        38: dict(link=('DEF-forearm.R.001','DEF-forefoot.R'), color=[255,0,0]),
+        39: dict(link=('DEF-forefoot.R','DEF-forefoot.R.001'), color=[255,0,0]),
+        40: dict(link=('DEF-forefoot.R.001','DEF-f_toe.R'), color=[255,0,0]),
+        41: dict(link=('DEF-f_toe.R','DEF-f_hoof.R'), color=[255,0,0]),
+        42: dict(link=('DEF-ear.L','DEF-ear.L.001'), color=[255,0,0]),
+        43: dict(link=('DEF-ear.R','DEF-ear.R.001'), color=[255,0,0]),
+        44: dict(link=('DEF-jaw','DEF-jaw.001'), color=[255,0,0]),
+        45: dict(link=('DEF-mane_base.01','DEF-mane_top.01'), color=[255,0,0]),
+        46: dict(link=('DEF-mane_base.02','DEF-mane_top.02'), color=[255,0,0]),
+        47: dict(link=('DEF-mane_base.03','DEF-mane_top.03'), color=[255,0,0]),
+        48: dict(link=('DEF-mane_base.04','DEF-mane_top.04'), color=[255,0,0]),
+        49: dict(link=('DEF-mane_base.05','DEF-mane_top.05'), color=[255,0,0]),
+        50: dict(link=('DEF-mane_base.06','DEF-mane_top.06'), color=[255,0,0]),
+    },
+
+    # ---- Weights / sigmas ----
+    joint_weights=[1.0] * 80,
+    sigmas=[0.025] * 80,
+
+    # ---- Body splits (kan fyllas i mer noggrant senare) ----
+    upper_body_ids=[
+        0,1,2,3,4,5,6,7,8,          # bröst, öron, ögon
+        13,14,15,16,                # underarmar
+        17,18,19,20,                # framfötter
+        21,26,27,                   # huvud/jaw
+        32,33,34,35,36,37,          # mane_base
+        38,39,40,41,42,43,          # mane_top
+        44,45,46,47,                 # nacke
+        48,49,                       # nos
+        56,57,                       # skuldror
+        58,59,60,                    # skalle
+        61,62,63,64,65,66,           # ryggrad
+        67                           # svans början
+    ],
+
+    lower_body_ids=[
+        9,10,11,12,                  # framhovar/tår
+        22,23,24,25,                 # bakfötter
+        28,29,30,31,                 # underben bak
+        50,51,                       # bäcken
+        52,53,54,55,                 # bakhovar/tår
+        72,73,74,75,                 # lår
+        76,77,78,79,                 # överarmar
+        68,69,70,71                  # svanssegment
+    ]
+
+)
